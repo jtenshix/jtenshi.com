e405f293624166a8ff0737e87977e94a102aa0e9
jtenshix <jtenshix@gmail.com>
2025-09-14 01:26:32 -0300

    Create get-playlist.js

---
+++ api/get-playlist.js
@@ -0,0 +1,24 @@
+import fetch from 'node-fetch';
+
+export default async function handler(request, response) {
+    const { playlistId } = request.query;
+
+    if (!playlistId) {
+        return response.status(400).json({ error: 'playlistId es un parámetro requerido.' });
+    }
+
+    try {
+        const apiKey = process.env.key_api;
+        const apiResponse = await fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistId}&key=${apiKey}&maxResults=50`);
+        
+        if (!apiResponse.ok) {
+            throw new Error(`Error de la API de YouTube: ${apiResponse.statusText}`);
+        }
+
+        const data = await apiResponse.json();
+        return response.status(200).json(data);
+
+    } catch (error) {
+        return response.status(500).json({ error: error.message });
+    }
+}
